72c646ce2a71192270e6355bdff2c34c5b97c459
aryan-95 <aryanpaswan801@gmail.com>
2026-08-14 15:59:11 +0530

    new files

---
+++ terraform/provider.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hasicrop/aws"
+        version = "~> 5.40"
+
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "enviroment" {
+    type = "string"
+    description = "ec2-instance"
+    default = "t3.micro"
+    sensitive = false
+}
+
+variable "vpc" {
+    
+  
+}
